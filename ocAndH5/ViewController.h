456802b396ac1171ad0539507ed05e2b67ae2c4f
zhangxin01 <zhangxin01@ainirobot.com>
2018-12-29 22:44:52 +0800

    Initial Commit

---
+++ ocAndH5/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ocAndH5
+//
+//  Created by zhangxin on 2018/12/29.
+//  Copyright © 2018年 zhangxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ocAndH5/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ocAndH5
+//
+//  Created by zhangxin on 2018/12/29.
+//  Copyright © 2018年 zhangxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
